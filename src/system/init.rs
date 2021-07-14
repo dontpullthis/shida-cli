35914ca5af4d6df15ac97d23b083ef0d71347353
Sergei Belyakov <1899416+tasmanianfox@users.noreply.github.com>
2021-07-14 22:22:54 +0300

    Logging format updated

--- src/system/init.rs
+++ src/system/init.rs
@@ -1,9 +1,16 @@
 use env_logger::Env;
+use env_logger::fmt::Color;
+use std::io::Write;
 
 pub fn init_logger() {
     let env = Env::new().filter("LOG_LEVEL").write_style("LOG_STYLE");
-    match env_logger::try_init_from_env(env) {
-        Ok(_) => {},
-        Err(_) => panic!("Failed to init logger."),
-    }
+    env_logger::builder()
+        .parse_env(env)
+        .format(|buf, record| {
+            let mut level_style = buf.style();
+            level_style.set_color(Color::Blue).set_bold(true);
+
+            writeln!(buf, "[{} {}] {}", buf.timestamp(), level_style.value(record.level()), record.args())
+        })
+        .init();
 }
